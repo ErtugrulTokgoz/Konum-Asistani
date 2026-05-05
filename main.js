956359ab4147d64dfb7506ad82451eed2110d5ce
ErtugrulTokgoz <ertugrultokgoz25@gmail.com>
2026-05-05 15:12:32 +0300

    Cafe & Restoran arama sorgusu optimize edildi (isim araması kaldırıldı, cuisine eklendi)

--- main.js
+++ main.js
@@ -254,13 +254,13 @@ function fetchPlaces(type) {
             break;
         case 'restaurant':
             var rFood = 4000;
-            // 1. Temel yeme-içme kategorileri
+            // 1. Temel yeme-içme kategorileri (restaurant, fast_food, cafe vb.)
             var qAmenity = nwr('["amenity"~"restaurant|cafe|fast_food|food_court|ice_cream"]', rFood, latF, lngF);
-            // 2. Küçük büfeler, şarküteriler ve dükkanlar
+            // 2. Küçük esnaflar ve büfeler (shop=kiosk, shop=convenience vb.)
             var qShop = nwr('["shop"~"kiosk|convenience|deli|pastry|bakery"]', rFood, latF, lngF);
-            // 3. İsim bazlı arama (dönerci, büfe vb. kaçırmamak için)
-            var qKeywords = nwr('[name~"döner|kebap|büfe|dürüm|tost|pide|lahmacun",i]', rFood, latF, lngF);
-            q = qAmenity + qShop + qKeywords;
+            // 3. Mutfak tipine göre (kebab, doner vb. etiketlenmiş yerler)
+            var qCuisine = nwr('["cuisine"~"kebab|doner|turkish|pizza|burger"]', rFood, latF, lngF);
+            q = qAmenity + qShop + qCuisine;
             break;
         case 'supermarket':
             q = nwr('["shop"~"supermarket|convenience"]', r, latF, lngF);
